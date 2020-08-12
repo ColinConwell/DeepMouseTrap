198919b6f5ca6e57fa4b23b235478a86c8cccc3f
ColinConwell <conwell@g.harvard.edu>
2020-08-12 12:56:41 -0400

    versioning

--- regression/neural_regression.py
+++ regression/neural_regression.py
@@ -11,42 +11,32 @@ from scipy.stats import pearsonr
 
 ### PLS Regression
 
-def kfold_pls_regression(X, y, n_splits = 6, n_components = 25):
-    scores, kfolds = [], KFold(n_splits, shuffle=False)
-    for train_indices, test_indices in kfolds.split(np.arange(119)):
+def kfold_pls_regression(X, y, n_splits = 6, n_components = 25, score_by_fold=True):
+    kfolds, scores, pred_y = KFold(n_splits, shuffle=False), [], np.zeros(y.shape[0])
+    for train_indices, test_indices in kfolds.split(np.arange(y.shape[0])):
         train_X, test_X = X[train_indices, :], X[test_indices, :]
         train_y, test_y = y[train_indices], y[test_indices]
         regression = PLSRegression(n_components).fit(train_X, train_y)
-        pred_y = regression.predict(test_X)
-        scores.append(pearsonr(pred_y.squeeze(), test_y)[0]**2)
-    return(np.mean(np.array(scores)))
-
-def brain_data_pls_regression(feature_map, brain_data, n_components):
-    score_sheet = []
-    X = feature_map
-    y_array = brain_data.transpose()
-    for y_i, y in enumerate(tqdm(y_array, leave=0)):
-        score = kfold_pls_regression(X,y,n_components=n_components)
-        score_sheet.append({'neuron': y_i, 'score': score})
-    return(pd.DataFrame(score_sheet))
+        pred_test_y = regression.predict(test_X).squeeze()
+        scores.append(pearsonr(pred_test_y, test_y)[0]**2)
+        pred_y[test_indices] = pred_test_y
+    if score_by_fold:
+        return(np.mean(np.array(scores)))
+    if not score_by_fold:
+        return(pearsonr(pred_y, y)[0]**2)
 
 # Ridge Regression
 
-def kfold_ridge_regression(X, y, n_splits = 6):
-    scores, kfolds = [], KFold(n_splits, random_state = 1)
-    for train_indices, test_indices in kfolds.split(np.arange(119)):
+def kfold_ridge_regression(X, y, n_splits = 6, score_by_fold=True):
+    kfolds, scores, pred_y = KFold(n_splits, shuffle=False), [], np.zeros(y.shape[0])
+    for train_indices, test_indices in kfolds.split(np.arange(y.shape[0])):
         train_X, test_X = X[train_indices, :], X[test_indices, :]
         train_y, test_y = y[train_indices], y[test_indices]
         regression = Ridge(alpha=1.0).fit(train_X, train_y)
-        pred_y = regression.predict(test_X)
-        scores.append(pearsonr(pred_y.squeeze(), test_y)[0]**2)
-    return(np.mean(np.array(scores)))
-
-def brain_data_ridge_regression(feature_map, brain_data):
-    score_sheet = []
-    X = feature_map
-    y_array = brain_data.transpose()
-    for y_i, y in enumerate(tqdm(y_array, leave=0)):
-        score = kfold_ridge_regression(X,y)
-        score_sheet.append({'neuron': y_i, 'score': score})
-    return(pd.DataFrame(score_sheet))
+        pred_test_y = regression.predict(test_X).squeeze()
+        scores.append(pearsonr(pred_test_y, test_y)[0]**2)
+        pred_y[test_indices] = pred_test_y
+    if score_by_fold:
+        return(np.mean(np.array(scores)))
+    if not score_by_fold:
+        return(pearsonr(pred_y, y)[0]**2)
