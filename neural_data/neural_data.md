856cde3f4a898446d7790e74a43448bb9a3a7795
ColinConwell <conwell@g.harvard.edu>
2020-08-28 00:58:01 -0400

    updates

---
+++ neural_data/neural_data.md
@@ -0,0 +1,9 @@
+#### Allen Brain Observatory Visual Coding Data
+
+The primary neural data in this dataset is an optical physiology dataset from the Allen Brain Observatory, called the *Visual Coding* dataset. The dataset consists of neural responses from approximately 39,000 visual cortical neurons to 119 natural scenes. More information on using and downloading the data may be found in the links below.
+
+[AllenSDK Docs](https://allensdk.readthedocs.io/en/latest/brain_observatory.html)
+
+[S3 Bucket for Raw Allen Institute Data](https://console.aws.amazon.com/s3/buckets/allen-brain-observatory/)
+
+[Wiki for Allen Institute Data on AWS](https://github.com/AllenInstitute/AllenSDK/wiki/Use-the-Allen-Brain-Observatory-%E2%80%93-Visual-Coding-on-AWS)
--- regression/results_processing.py
+++ regression/results_processing.py
@@ -1,6 +1,20 @@
-import os, sys
+import os, sys, argparse
 import numpy as np
 import pandas as pd
+from glob import glob
+from tqdm.auto import tqdm as tqdm
+
+def max_transform(df, group_vars, measure_var = 'score', deduplicate=True):
+    if not isinstance(group_vars, list):
+        group_vars = list(group_vars)
+    
+    max_df = (df[df.groupby(group_vars)[measure_var]
+                 .transform(max) == df[measure_var]]).reset_index(drop=True)
+                 
+    if deduplicate:
+        max_df = max_df[~max_df.duplicated(group_vars + [measure_var])]
+        
+    return max_df
 
 if __name__ == "__main__":
 
@@ -9,17 +23,28 @@ if __name__ == "__main__":
                         help='directory with regression results')
     parser.add_argument('--output_dir', type=str, required=False, default='./',
                         help='directory for output from random projections')
-    parser.add_argument('--target_output', type=str, required=False, default='./',
+    parser.add_argument('--target_output', type=str, required=False, default='max',
                         help='directory for output from random projections')
+    
+    args = parser.parse_args()
+    input_dir = args.input_dir
+    output_dir = args.output_dir
+    target_output = args.target_output
 
-
-source_dir = 'nu_srp_ridge_results'
-
-asset_dflist = []
-for asset_dir in tqdm(os.listdir(output_dir)):
-    if 'taskonomy' not in asset_dir:
-        incoming_assets = glob(os.path.join(output_dir, asset_dir + '/*.csv'))
+    print('Loading results...')
+    asset_dflist = []
+    for asset_dir in tqdm(os.listdir(input_dir)[:3]):
+        incoming_assets = glob(os.path.join(input_dir, asset_dir + '/*.csv'))
         for asset in tqdm(incoming_assets, leave=False):
             asset_dflist.append(pd.read_csv(asset))
+
+    print('Concatenating results...')
+    scoresheet = pd.concat(asset_dflist)
     
-scoresheet = pd.concat(asset_dflist)
+    print('Parsing results by {}...'.format(target_output))
+    
+    if target_output == 'max':
+        output_df = max_transform(scoresheet, group_vars = ['cell_specimen_id', 'model', 'train_type'])
+        output_df.to_csv(os.path.join(output_dir, '_'.join(input_dir.split('/')) + '.csv'), index = None)
+        
+        
