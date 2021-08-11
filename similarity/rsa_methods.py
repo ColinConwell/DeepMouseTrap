2eb1ca0b8057153eb673c3035eb3688a16d28fe3
ColinConwell <conwell@g.harvard.edu>
2021-08-11 14:42:53 -0400

    simplifying nonnegative linear regression

--- similarity/rsa_methods.py
+++ similarity/rsa_methods.py
@@ -1,16 +1,8 @@
 import numpy as np
 import pandas as pd
 
-from scipy import sparse
-from scipy.optimize import OptimizeResult, nnls
-from sklearn.utils import check_X_y
-from sklearn.base import RegressorMixin
-from sklearn.exceptions import ConvergenceWarning
-from sklearn.utils.extmath import safe_sparse_dot
-from sklearn.linear_model import LinearRegression
-
+from sklearn.linear_model import LinearRegression, ElasticNet
 from sklearn.model_selection import KFold, RepeatedKFold
-from sklearn.linear_model import ElasticNet
 from scipy.stats import pearsonr, spearmanr
 
 #### Standard Representational Similarity --------------------------------------------------
@@ -26,98 +18,21 @@ def compare_rdms(rdm1, rdm2, dist_type = 'pearson'):
 
 #### Representational Similarity Regression --------------------------------------------------
 
-#source: https://github.com/civisanalytics/civisml-extensions/blob/master/civismlext/nonnegative.py
-class NonNegativeLinearRegression(LinearRegression, RegressorMixin):
-    """Non-negative least squares linear regression.
+def kfold_nonnegative_regression(target_rdm, model_rdms, regression_type='linear', 
+                                 n_splits=10, n_repeats=None, random_state=None):
+    '''Non-negative least squares linear regression on RDMs with k-fold cross-validation.
     Parameters
     ----------
-    fit_intercept : boolean, optional
-        whether to calculate the intercept for this model. If set
-        to false, no intercept will be used in calculations
-        (e.g. data is expected to be already centered).
-    normalize : boolean, optional, default False
-        If True, the regressors X will be normalized before regression.
-        This parameter is ignored when `fit_intercept` is set to False.
-        When the regressors are normalized, note that this makes the
-        estimated coefficients more robust and almost independent of the
-        number of samples. The same property is not valid for standardized
-        data. However, if you wish to standardize, please use
-        `preprocessing.StandardScaler` before calling `fit` on an estimator
-        with `normalize=False`.
-    copy_X : boolean, optional, default True
-        If True, X will be copied; else, it may be overwritten.
+    target_rdm: your brain data RDM (n_samples x n_samples)
+    model_rdms: your model layer RDMs (n_samples x n_samples x n_layers)
+    n_splits: how many cross_validated folds
+    n_repeats: how many times to perform k-fold splits
+    random_state: used if you want to use a particular set of random splits
     Attributes
     ----------
-    coef_ : array, shape (n_features, )
-        Estimated coefficients for the linear regression problem.
-    intercept_ : array
-        Independent term in the linear model.
-    opt_result_ : OptimizeResult
-        Result of non-negative least squares optimization
-    """
-
-    def __init__(self, fit_intercept=True, normalize=False, copy_X=True):
-        self.fit_intercept = fit_intercept
-        self.normalize = normalize
-        self.copy_X = copy_X
-        
-    def _rescale_data(X, y, sample_weight):
-        """Rescale data so as to support sample_weight"""
-        n_samples = X.shape[0]
-        sample_weight = sample_weight * np.ones(n_samples)
-        sample_weight = np.sqrt(sample_weight)
-        sw_matrix = sparse.dia_matrix((sample_weight, 0),
-                                      shape=(n_samples, n_samples))
-        X = safe_sparse_dot(sw_matrix, X)
-        y = safe_sparse_dot(sw_matrix, y)
-        return X, y
-
-    def fit(self, X, y, sample_weight=None, zero_coef_alert=True):
-        """Fit non-negative linear model.
-        Parameters
-        ----------
-        X : numpy array or sparse matrix of shape [n_samples, n_features]
-            Training data
-        y : numpy array of shape [n_samples,]
-            Target values
-        sample_weight : numpy array of shape [n_samples]
-            Individual weights for each sample
-        Returns
-        -------
-        self : returns an instance of self.
-        """
-        X, y = check_X_y(X, y, y_numeric=True, multi_output=False)
-
-        if sample_weight is not None and np.atleast_1d(sample_weight).ndim > 1:
-            raise ValueError("Sample weights must be 1D array or scalar")
-
-        X, y, X_offset, y_offset, X_scale = self._preprocess_data(
-            X, y, fit_intercept=self.fit_intercept, normalize=self.normalize,
-            copy=self.copy_X, sample_weight=sample_weight)
-
-        if sample_weight is not None:
-            # Sample weight can be implemented via a simple rescaling.
-            X, y = _rescale_data(X, y, sample_weight)
-
-        self.coef_, result = nnls(X, y.squeeze())
-
-        if np.all(self.coef_ == 0) and zero_coef_alert == True:
-            raise ConvergenceWarning("All coefficients estimated to be zero in"
-                                     " the non-negative least squares fit.")
-
-        self._set_intercept(X_offset, y_offset, X_scale)
-        self.opt_result_ = OptimizeResult(success=True, status=0, x=self.coef_,
-                                          fun=result)
-        return self
-
-def kfold_nonnegative_regression(target_rdm, model_rdms, regression_type='linear', n_splits=10, n_repeats=None, 
-                              random_state=None, zero_coef_alert=False):
-    '''
-        target_rdm: your brain data RDM (n_samples x n_samples)
-        model_rdms: your model layer RDMs (n_samples x n_samples x n_layers)
-        standardize: whether to standardize features for regression (see NNLSRegression)
-        n_splits: how many cross_validated folds
-        random_state: used if you want to use a particular set of random splits
+    r : correlation between predicted and actual RDM
+    coefficients : the coefficients across k-fold splits
+    intercepts : the intercepts across k-fold splits
     '''
     n_items = target_rdm.shape[0]
     
@@ -150,8 +65,8 @@ def kfold_nonnegative_regression(target_rdm, model_rdms, regression_type='linear
 
         # fit the regression model
         if regression_type == 'linear':
-            regression = NonNegativeLinearRegression(fit_intercept=True, normalize=False)
-            regression.fit(X_train, y_train, zero_coef_alert=zero_coef_alert)
+            regression = LinearRegression(fit_intercept=True, positive=True)
+            regression.fit(X_train, y_train)
         if regression_type == 'elastic_net':
             regression = ElasticNet(alpha = 1.0, l1_ratio = 0, positive = True)
             regression.fit(X_train, y_train)
@@ -195,3 +110,15 @@ def max_transform(df, group_vars, measure_var = 'score', deduplicate=True):
         max_df = max_df[~max_df.duplicated(group_vars + [measure_var])]
         
     return max_df
+
+def min_transform(df, group_vars, measure_var = 'score', deduplicate=True):
+    if not isinstance(group_vars, list):
+        group_vars = list(group_vars)
+    
+    min_df = (df[df.groupby(group_vars)[measure_var]
+                 .transform(min) == df[measure_var]]).reset_index(drop=True)
+                 
+    if deduplicate:
+        min_df = min_df[~min_df.duplicated(group_vars + [measure_var])]
+        
+    return min_df
